b165b228977f96f02cdd93b2ac8f38d472630a23
makesitlookeasy <kenmrosenberg@comcast.net>
2017-01-11 10:49:09 +1300

    start over add just sprites for now

--- ArmyIntelligence/js/main.js
+++ ArmyIntelligence/js/main.js
@@ -3,48 +3,63 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload() {
 
+    game.stage.backgroundColor = '#85b5e1';
+
+    game.load.baseURL = 'http://examples.phaser.io/assets/';
+    game.load.crossOrigin = 'anonymous';
+
+    game.load.image('player', 'sprites/phaser-dude.png');
+    game.load.image('platform', 'sprites/platform.png');
+
     game.load.image('orb-blue', 'assets/sprites/orb-blue.png', 32, 32);
     game.load.image('orb-red', 'assets/sprites/orb-red.png', 32, 32);
 
 }
 
+var player;
+var platforms;
+var cursors;
+var jumpButton;
 var reds;
 var blues;
 
 function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    reds = game.add.group();
-    blues = game.add.group();
-    reds.enableBody = true;
-    blues.enableBody = true;
+    player = game.add.sprite(100, 200, 'player');
 
-    for(i = 0; i < 5; i++){
-        var red = reds.create(game.world.randomX, game.world.randomY, 'orb-red');
-        var blue = blues.create(game.world.randomX, game.world.randomY, 'orb-blue');
-    }
+    game.physics.arcade.enable(player);
 
-    /*
-    red = game.add.sprite(100, 100, 'orb-red');
-    game.physics.enable(red, Phaser.Physics.ARCADE);
-    blue = game.add.sprite(400, 300, 'orb-blue');
-    game.physics.enable(blue, Phaser.Physics.ARCADE);
-    blue.body.velocity.setTo(game.rnd.integerInRange(-50,50), game.rnd.integerInRange(-50,50));
-    */
+    player.body.collideWorldBounds = true;
+    player.body.gravity.y = 500;
+
+    platforms = game.add.physicsGroup();
+
+    platforms.create(500, 150, 'platform');
+    platforms.create(-200, 300, 'platform');
+    platforms.create(400, 450, 'platform');
+
+    platforms.setAll('body.immovable', true);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
 function update() {
+    game.physics.arcade.collide(player, platforms);
 
-        reds.forEach(game.physics.arcade.moveToPointer, game.physics.arcade, false);
+    player.body.velocity.x = 0;
 
-    /*
-    game.physics.arcade.moveToObject(red, blue, 50, 500);
-    if(game.physics.arcade.distanceBetween(red, blue) < 25){
-        game.stage.backgroundColor = Phaser.Color.getRandomColor(50, 255, 255);
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -250;
     }
-    */
-}
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 250;
+    }
 
-function moveToNearestEnemy(){
-
+    if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down))
+    {
+        player.body.velocity.y = -400;
+    }
 }
