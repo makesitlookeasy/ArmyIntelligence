f4c775a70d1c8ebb08319562eadb9bf686d51717
makesitlookeasy <kenmrosenberg@comcast.net>
2017-01-18 14:32:00 +1300

    Added a score readout

--- ArmyIntelligence/js/main.js
+++ ArmyIntelligence/js/main.js
@@ -5,13 +5,12 @@ function preload() {
 
     game.stage.backgroundColor = '#85b5e1';
 
-    game.load.baseURL = 'http://examples.phaser.io/assets/';
-    game.load.crossOrigin = 'anonymous';
+    game.load.image('player', 'assets/sprites/phaser-dude.png');
+    game.load.image('platform', 'assets/sprites/platform.png');
+    game.load.image('arrow', 'assets/sprites/arrow.png');
+    game.load.image('orb', 'assets/sprites/orb-green.png');
 
-    game.load.image('player', 'sprites/phaser-dude.png');
-    game.load.image('platform', 'sprites/platform.png');
-    game.load.image('arrow', 'sprites/arrow.png');
-    game.load.image('orb', 'sprites/orb-green.png');
+    game.load.bitmapFont('gem', 'assets/fonts/bitmapFonts/gem.png', 'assets/fonts/bitmapFonts/gem.xml');
 
 }
 
@@ -29,40 +28,41 @@ var timeElapsed;
 var buttonState;
 var movingLeft;
 var movingRight;
+var score;
+var scoreText;
 
 
 function create() {
 
+    // Player Sprite
     player = game.add.sprite(100, 200, 'player');
-
     game.physics.arcade.enable(player);
-
     player.body.collideWorldBounds = true;
     player.body.gravity.y = 500;
 
+    // Platforms
     platforms = game.add.physicsGroup();
-
     platforms.create(500, 150, 'platform');
     platforms.create(0, 300, 'platform');
-
     platforms.setAll('body.immovable', true);
 
     cursors = game.input.keyboard.createCursorKeys();
     jumpKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     // Buttons
+        // Right Button
     rightButton = game.add.button(200,  game.world.height - 150, 'arrow', null, this);
     rightButton.scale.setTo(2,2);
     rightButton.onInputDown.add(rightDown, this);
     rightButton.onInputUp.add(rightUp, this);
-
+        // Left Button
     leftButton = game.add.button(50 ,  game.world.height - 150, 'arrow', null, this)
     leftButton.anchor.setTo(1,1);
     leftButton.angle = 180;
     leftButton.scale.setTo(2,2);
     leftButton.onInputDown.add(leftDown, this);
     leftButton.onInputUp.add(leftUp, this);
-
+        // Jump Button
     jumpButton = game.add.button(600, game.world.height - 150, 'orb', jumpClick, this);
     jumpButton.scale.setTo(4,4);
 
@@ -71,9 +71,15 @@ function create() {
     buttonState = 0;
     movingLeft = false;
     movingRight = false;
+
+    score = 0;
+    scoreText = game.add.bitmapText(0, 0, 'gem', 'SCORE: ', 64);
 }
 
 function update() {
+
+    score++;
+    scoreText.text = 'SCORE: ' + score;
 
     game.physics.arcade.collide(player, platforms);
     player.body.velocity.x = 0;
