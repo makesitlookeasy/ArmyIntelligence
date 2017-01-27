e866690c1e1637bfa108c9c163ffb392505e5ff1
mjgallo <mjgallo@udel.edu>
2017-01-27 21:27:25 +1300

    why to play

--- FlapFlop/www/js/why.js
+++ FlapFlop/www/js/why.js
@@ -7,21 +7,6 @@ var whyState = {
         backButton2 = game.add.button(backx, backy, 'back', null, this);
         backButton2.onInputDown.add(backClick2, this);
         backButton2.onInputUp.add(backRelease2, this);
-
-        /*var keyboardButtonx = game.width/2 - game.cache.getImage('start').width/2 - 100;
-        var keyboardButtony = 100;
-        keyboardButton = game.add.button(keyboardButtonx, keyboardButtony, 'KeyboardButton', null, this);
-        keyboardButton.onInputDown.add(keyboardClick, this);
-        keyboardButton.onInputUp.add(keyboardRelease, this);
-
-        var whyx = keyboardButtonx;
-        var whyy = keyboardButtony + 120;
-        whyButton = game.add.button(whyx, whyy, 'back',null, this);
-        whyButton.onInputDown.add(whyClick, this);
-        whyButton.onInputUp.add(whyRelease, this);
-
-        checkmark = game.add.sprite(600, 80, 'checkmark');
-        checkmark.scale.set(0.5);*/
     }
 };
 
